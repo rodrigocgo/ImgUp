e3202422417e1d81e3da448c02acb239ab53ea70
rodrigo.gon95 <rodrigo.gon95@gmail.com>
2020-06-30 17:55:04 -0300

    Adicionar arquivos de projeto.

---
+++ ImgUp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImgUp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
